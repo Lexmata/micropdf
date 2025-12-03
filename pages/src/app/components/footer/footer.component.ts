da793ca9d7fc5f2809abba8e66fbdf3574cbd093
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2025-12-03 14:53:33 -0500

    feat(pages): add Angular FontAwesome for icon rendering
    - Install @fortawesome/angular-fontawesome and icon packages
    - Update header component with GitHub, menu, close icons
    - Update footer component with GitHub, Rust, npm icons
    - Use SVG icons for better performance and scalability

--- pages/src/app/components/footer/footer.component.ts
+++ pages/src/app/components/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faNpm, faRust } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
+  
+  // Icons
+  faGithub = faGithub;
+  faNpm = faNpm;
+  faRust = faRust;
 }
--- pages/src/app/components/header/header.component.ts
+++ pages/src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   menuOpen = signal(false);
+  
+  // Icons
+  faGithub = faGithub;
+  faBars = faBars;
+  faXmark = faXmark;
 }
